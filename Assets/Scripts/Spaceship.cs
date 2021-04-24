9b790a8dd037fe88e70cbebd3030d1b85e4c743c
Sergeyxow <Sergeyxow@yandex.ru>
2021-04-24 15:22:20 +0300

    spaceship

---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DefaultNamespace
+{
+    public class Spaceship : MonoBehaviour
+    {
+        [SerializeField] private MoverBase _mover;
+        [SerializeField] private PlayerInput _playerInput;
+
+        private void Update()
+        {
+            Vector2 movementInput = _playerInput.currentActionMap["movement"].ReadValue<Vector2>();
+            _mover.AddForce(movementInput * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/VelocityMover.cs
+++ Assets/Scripts/VelocityMover.cs
@@ -7,6 +7,9 @@ namespace DefaultNamespace
     {
         [SerializeField] private float _drag;
         [SerializeField] private float _angularDrag;
+
+        [SerializeField] private Vector2 _velocityLimit;
+        [SerializeField] private Vector2 _angularVelocityLimit;
         
         private Vector2 _velocity;
         private Vector2 _angularVelocity;
@@ -23,8 +26,11 @@ namespace DefaultNamespace
 
         private void Update()
         {
-            _velocity = Vector2.Lerp(_velocity, Vector2.zero, _drag);
-            _angularVelocity = Vector2.Lerp(_angularVelocity, Vector2.zero, _angularDrag);
+            transform.position += (Vector3)_velocity * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + (Vector3) _angularVelocity * Time.deltaTime);
+
+            _velocity = Vector2.Lerp(_velocity, Vector2.zero, _drag * Time.deltaTime);
+            _angularVelocity = Vector2.Lerp(_angularVelocity, Vector2.zero, _angularDrag * Time.deltaTime);
         }
     }
 }
