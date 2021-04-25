52d19498b6b40e4c34fe8b07d72c24ba326bcac5
Sergeyxow <Sergeyxow@yandex.ru>
2021-04-25 15:06:34 +0300

    Level bounds

---
+++ Assets/Scripts/DestroyOnHit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DestroyOnHit : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
