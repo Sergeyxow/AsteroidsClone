8b40998cf2dcecf3d33ae371b4c2a71cde775425
Sergeyxow <Sergeyxow@yandex.ru>
2021-04-25 16:51:40 +0300

    Game manager and score counter

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DefaultNamespace
+{
+    public class GameManager : MonoBehaviour
+    {
+        public UnityEvent<int> ScoreUpdated;
+        
+        private int _score = 0;
+
+        private void Start()
+        {
+            ScoreUpdated?.Invoke(_score);
+        }
+
+        public void IncreaseScore(int value)
+        {
+            _score += value;
+            ScoreUpdated?.Invoke(_score);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ScoreCounter.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace.UI
+{
+    public class ScoreCounter : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _textBox;
+
+        public void ChangeScore(int score)
+        {
+            _textBox.text = "Score: " + score;
+        }
+    }
+}
